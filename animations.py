591ccf9180ba5e0eaa2f08816ecf880c984bd798
drewstephenson <116836139+drewstephenson@users.noreply.github.com>
2024-06-08 16:13:57 -0500

    Update animations.py

--- animations.py
+++ animations.py
@@ -11,17 +11,14 @@ class Animation:
         # Dictionary of lists, each one contains frames for each of Bonzi's animations
         # glob returns a list of all .bmp files in the specified directory, sorted puts them in numeric order
         self.animations = {
-            "idle": sorted(glob.glob("animations/idle/*.bmp")),
-            "arrive": sorted(glob.glob("animations/arrive/*.bmp")),
-            "goodbye": sorted(glob.glob("animations/goodbye/*.bmp")),
-            "backflip": sorted(glob.glob("animations/backflip/*.bmp")),
-            "glasses": sorted(glob.glob("animations/glasses/*.bmp")),
-            "laughing": sorted(glob.glob("animations/laughing/*.bmp")),
-            "sleeping": sorted(glob.glob("animations/sleeping/*.bmp")),
-            "thinking": sorted(glob.glob("animations/thinking/*.bmp")),
-            "wave": sorted(glob.glob("animations/wave/*.bmp")),
-            "nothing": ["animations/idle//0999.bmp"],
-            "talking": sorted(glob.glob("animations/talking/*"))
+            "idle": sorted(glob.glob("idle/*.bmp")),
+            "arrive": sorted(glob.glob("arrive/*.bmp")),
+            "goodbye": sorted(glob.glob("goodbye/*.bmp")),
+            "backflip": sorted(glob.glob("backflip/*.bmp")),
+            "glasses": sorted(glob.glob("glasses/*.bmp")),
+            "wave": sorted(glob.glob("wave/*.bmp")),
+            "nothing": ["idle/0999.bmp"],
+            "talking": sorted(glob.glob("talking/*"))
         }
 
     def get_animation(self, command):
